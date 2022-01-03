cfc4a9448b9f1a1a9a1f5c252a4731778210d886
Taptrue <82027635+Taptrue1@users.noreply.github.com>
2022-01-04 01:56:45 +0300

    Отрефакторил некоторые методы и добавил новый функционал
    Добавлены
    -метод слияния с другой ячейкой
    -метод передвижения в другую ячейку
    -метод увеличения value

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,31 +1,51 @@
+using System;
 using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
 
 public class Cell : MonoBehaviour
 {
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
     public int Value { get; private set; }
     public int Points => IsEmpty ? 0 : (int)Mathf.Pow(2, Value);
     public bool IsEmpty => Value == 0;
+    public bool IsMerged { get; private set; }
 
     [SerializeField] private Image _image;
     [SerializeField] private TextMeshProUGUI _pointsText;
     [SerializeField] private ColorsConfig _colorsConfig;
 
+    private Action<int> _valueChanged;
     private const int _maxValue = 11;
 
-    private void Awake()
+    public void Init(int x, int y, Action<int> valueChanged)
     {
+        X = x;
+        Y = y;
+        _valueChanged = valueChanged;
+    }
+    public void MergeWithCell(Cell target)
+    {
+        if (target = this)
+            throw new System.Exception("Нельзя совместить плитку с самой собой");
+
+        target.IncreaseValue();
+        SetValue(0);
+
         UpdateCell();
     }
-
-    public void IncreaseValue()
+    public void MoveToCell(Cell target)
     {
-        if (Value < _maxValue)
-        {
-            Value++;
-            UpdateCell();
-        }
+        target.SetValue(Value);
+        SetValue(0);
+
+        UpdateCell();
+    }
+    public void ResetFlags()
+    {
+        IsMerged = false;
     }
     public void SetValue(int value)
     {
@@ -33,6 +53,16 @@ public class Cell : MonoBehaviour
         UpdateCell();
     }
 
+    private void IncreaseValue()
+    {
+        if (Value < _maxValue)
+        {
+            Value++;
+            IsMerged = true;
+            _valueChanged?.Invoke(Points);
+            UpdateCell();
+        }
+    }
     private void UpdateCell()
     {
         _pointsText.text = IsEmpty ? string.Empty : Points.ToString();
