2c0b86eb988e738345a9d413e97c95e04c5fc806
Taptrue <82027635+Taptrue1@users.noreply.github.com>
2022-01-04 01:55:27 +0300

    Добавил функционал классу Game

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,25 +1,69 @@
 using UnityEngine;
+using TMPro;
 
 public class Game : MonoBehaviour
 {
+    [Header("Dependencies")]
+    [SerializeField] private Area _area;
 
+    [Header("UI Options")]
+    [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private TextMeshProUGUI _resultText;
+    [SerializeField] private string _winText;
+    [SerializeField] private string _loseText;
+
+    private int _points;
+    private bool _isGameStarted;
     private Vector2 _startTouchPosition;
     private Vector2 _endSwipePosition;
-    private bool isSwiped;
 
+    private void Start()
+    {
+        _area.Init(OnWin, OnLose, AddPoints);
+        StartGame();
+    }
     private void Update()
     {
         GetInput();
         var swipeDirection = GetSwipeDirection();
+        if(_isGameStarted)
+        {
+            _area.OnInput(swipeDirection);
+        }
     }
 
+    private void StartGame()
+    {
+        _resultText.text = "";
+        _isGameStarted = true;
+        SetPoints(0);
+        _area.GenerateArea();
+    }
+    private void OnWin()
+    {
+        _resultText.text = _winText;
+    }
+    private void OnLose()
+    {
+        _resultText.text = _loseText;
+    }
+    private void AddPoints(int value)
+    {
+        if (value < 0) return;
+        SetPoints(_points + value);
+    }
+    private void SetPoints(int value)
+    {
+        _points = value;
+        _scoreText.text = value.ToString();
+    }
     private Vector2 GetSwipeDirection()
     {
         var direction = _endSwipePosition - _startTouchPosition;
 
         if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
             return direction.x > 0 ? Vector2.right : Vector2.left;
-        else
+        else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
             return direction.y > 0 ? Vector2.up : Vector2.down;
 
         return Vector2.zero;
