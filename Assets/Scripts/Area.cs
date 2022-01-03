8c34539001b4e3738e2521d72fc2402e15c522e2
Taptrue <82027635+Taptrue1@users.noreply.github.com>
2022-01-04 01:57:10 +0300

    Реализовал функционал перемещения ячеек и их слияния

--- Assets/Scripts/Area.cs
+++ Assets/Scripts/Area.cs
@@ -1,78 +1,178 @@
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(RectTransform))]
 public class Area : MonoBehaviour
 {
     [Header("Area Settings")]
-    [SerializeField] private int _columns;
-    [SerializeField] private int _rows;
+    [SerializeField] private int _areaSize;
     [SerializeField] private float _cellSize;
     [SerializeField] private float _spacing;
     [Header("Other")]
     [SerializeField] private int _startCellsCount;
     [SerializeField] private Cell _cellPrefab;
 
+    private Action _win;
+    private Action _lose;
+    private Action<int> _valueChanged;
     private RectTransform _rectTransform;
     private Cell[,] _area;
+    private bool _isAnyCellMoved;
 
     private void Awake()
     {
         _rectTransform = GetComponent<RectTransform>();
-
-        GenerateArea();
     }
 
-    public void GenerateArea()
+    public void Init(Action win, Action lose, Action<int> valueChanged)
+    {
+        _win = win;
+        _lose = lose;
+        _valueChanged = valueChanged;
+    }
+    public void OnInput(Vector2 direction)
+    {
+        _isAnyCellMoved = false;
+        ResetCellsFlags();
+
+        Move(direction);
+
+        if(_isAnyCellMoved)
+        {
+            GenerateCell();
+            CheckGameResult();
+        }
+    }
+    public  void GenerateArea()
     {
         if (_area == null)
             CreateArea();
 
-        for (int i = 0; i < _columns; i++)
-            for (int j = 0; j < _rows; j++)
-                _area[i, j].SetValue(0);
+        for (int y = 0; y < _areaSize; y++)
+            for (int x = 0; x < _areaSize; x++)
+                _area[y, x].SetValue(0);
 
         for (int i = 0; i < _startCellsCount; i++)
             GenerateCell();
     }
 
+    private void Move(Vector2 direction)
+    {
+        var startXY = direction.x > 0 || direction.y < 0 ? _areaSize - 1 : 0;
+        var dir = direction.x != 0 ? (int)direction.x : -(int)direction.y;
+
+        for(int i = 0; i < _areaSize; i++)
+        {
+            for(int k = startXY; k >= 0 && k < _areaSize; k -= dir)
+            {
+                var cell = direction.x != 0 ? _area[i, k] : _area[k, i];
+
+                if (cell.IsEmpty) continue;
+
+                var cellToMerge = FindCellToMerge(cell, direction);
+                if(cellToMerge != null)
+                {
+                    _isAnyCellMoved = true;
+                    cell.MergeWithCell(cellToMerge);
+
+                    continue;
+                }
+
+                var emptyCell = FindEmptyCell(cell, direction);
+                if(emptyCell != null)
+                {
+                    _isAnyCellMoved = true;
+                    cell.MoveToCell(emptyCell);
+                }
+            }
+        }
+    }
+    private void CheckGameResult()
+    {
+
+    }
+    private Cell FindCellToMerge(Cell cell, Vector2 direction)
+    {
+        var startX = cell.X + (int)direction.x;
+        var startY = cell.Y - (int)direction.y;
+
+        for (int x = startX, y = startY; x >= 0 && y >= 0 && x < _areaSize && y < _areaSize; x += (int)direction.x, y -= (int)direction.y)
+        {
+            if (_area[y, x].IsEmpty)
+                continue;
+
+            if (_area[y, x].Value == cell.Value && !_area[y, x].IsMerged)
+                return _area[y, x];
+
+            break;
+        }
+
+        return null;
+    }
+    private Cell FindEmptyCell(Cell cell, Vector2 direction)
+    {
+        Cell emptyCell = null;
+        var startX = cell.X + (int)direction.x;
+        var startY = cell.Y - (int)direction.y;
+
+        for (int x = startX, y = startY; x >= 0 && y >= 0 && x < _areaSize && y < _areaSize; x += (int)direction.x, y -= (int)direction.y)
+        {
+            if (_area[y, x].IsEmpty) 
+                emptyCell = _area[y, x];
+        }
+
+        return emptyCell;
+    }
+    private void ResetCellsFlags()
+    {
+        for(int y = 0; y < _areaSize; y++)
+        {
+            for(int x = 0; x < _areaSize; x++)
+            {
+                _area[y, x].ResetFlags();
+            }
+        }
+    }
     private void GenerateCell()
     {
         var emptyCells = new List<Cell>();
 
-        for (int i = 0; i < _columns; i++)
-            for (int j = 0; j < _rows; j++)
-                if (_area[i, j].IsEmpty)
-                    emptyCells.Add(_area[i, j]);
+        for (int y = 0; y < _areaSize; y++)
+            for (int x = 0; x < _areaSize; x++)
+                if (_area[y, x].IsEmpty)
+                    emptyCells.Add(_area[y, x]);
 
         if (emptyCells.Count == 0)
             throw new System.Exception("Нет свободных ячеек");
 
-        var value = Random.Range(0, 10) == 0 ? 2 : 1;
-        emptyCells[Random.Range(0, emptyCells.Count)].SetValue(value);
+        var value = UnityEngine.Random.Range(0, 10) == 0 ? 2 : 1;
+        emptyCells[UnityEngine.Random.Range(0, emptyCells.Count)].SetValue(value);
     }
     private void CreateArea()
     {
-        _area = new Cell[_columns, _rows];
+        _area = new Cell[_areaSize, _areaSize];
         SetAreaSize();
 
         var xPosition = -(_rectTransform.sizeDelta.x / 2) + (_cellSize / 2) + _spacing;
         var yPosition = (_rectTransform.sizeDelta.y / 2) - (_cellSize / 2) - _spacing;
         var offset = _cellSize + _spacing;
-        for (int y = 0; y < _columns; y++)
+
+        for (int y = 0; y < _areaSize; y++)
         {
-            for (int x = 0; x < _rows; x++)
+            for (int x = 0; x < _areaSize; x++)
             {
                 var cell = Instantiate(_cellPrefab, transform, false);
                 var position = new Vector2(xPosition + offset * x, yPosition - offset * y);
                 cell.transform.localPosition = position;
+                cell.Init(x, y, _valueChanged);
                 _area[y, x] = cell;
             }
         }
     }
     private void SetAreaSize()
     {
-        var size = new Vector2(_columns * (_cellSize + _spacing) + _spacing, _rows * (_cellSize + _spacing) + _spacing);
-        _rectTransform.sizeDelta = size;
+        var size = _areaSize * (_cellSize + _spacing) + _spacing; 
+        _rectTransform.sizeDelta = new Vector2(size, size);
     }
 }
